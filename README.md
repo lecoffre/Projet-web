6a01b18f19430af2ff5feb6ac5f6822492e21cb0
lecoffre <kamil.boudani@gmail.com>
2021-03-18 00:28:05 +0100

    clean master + branch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-projetweb
+Projet web
+branches (toutes) : 3 + master = 4
+branches (disponibles) : 1
+Branche 1: front end
+Branche 2: bdd
+branche 3: backend bdd + api 
+master : projet fini
--- js/main.js
+++
@@ -1,9 +0,0 @@
-//multiplechoix pour les delegues
-$(document).ready(function(){
-    var multipleCancelButton = new Choices('#droit_delegue', {
-    removeItemButton: true,
-    maxItemCount:2,
-    searchResultLimit:15,
-    renderChoiceLimit:15
-    });
-     });
